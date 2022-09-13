6f4a751f52a461f08525862dbb4b050547b740c6
Koralek <dkoralek2@gmail.com>
2022-09-13 09:00:28 +0200

    zadanie 10

---
+++ kurs cz 2/zadanie10-js/main.js
@@ -0,0 +1,44 @@
+// Zadanie 1
+
+// const name = 'Daria'
+// let age
+// age = 30
+// const favFood = 'kabanosy'
+
+// console.log(`Mam na imię ${name}, mam ${age} lat i bardzo lubię ${favFood}`);
+
+// Zadanie 2
+
+// const score = 15 % 2
+// console.log(score);
+
+// let num1 = 8
+// num1++
+// num1++
+// console.log(num1);
+
+// let x = 5
+// const y = 10
+
+// console.log(x*=y);
+// console.log(x/=y);
+
+// Zadanie 3
+
+const numbers = [1, 2, 3, 4, 5, 6]
+
+for (let i = 0; i < numbers.length; i++) {
+	console.log(numbers[i])
+}
+
+function test(x, y, z) {
+	console.log(x + y + z)
+}
+
+test(1, 2, 3)
+
+if(numbers.length > 5) {
+console.log('TRUE')
+} else {
+    console.log('FALSE')
+}
