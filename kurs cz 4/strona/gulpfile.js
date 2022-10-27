fc3f5f6b85b7bf860e5e5001a9f79aad355380e0
Koralek <dkoralek2@gmail.com>
2022-10-27 15:23:05 +0200

    usatwienia

---
+++ kurs cz 4/strona/gulpfile.js
@@ -0,0 +1,64 @@
+const { src, dest, series, parallel } = require('gulp')
+
+const sass = require('gulp-sass')(require('sass'))
+
+const cssnano = require('gulp-cssnano')
+const autoprefixer = require('gulp-autoprefixer')
+const rename = require('gulp-rename')
+const babel = require('gulp-babel')
+
+const uglify = require('gulp-uglify')
+const imagemin = require('gulp-imagemin')
+
+const sourcemaps = require('gulp-sourcemaps')
+
+const browserSync = require('browser-sync').create();
+
+const paths = {
+	sass: './src/sass/**/*.scss',
+	js: './src/js/**/*.js',
+	img: './src/img/*',
+	sassDest: './dist/css',
+	jsDest: './dist/js',
+	imgDest: './dist/img',
+}
+
+function sassCompiler(done) {
+	src(paths.sass)
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(cssnano())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(sourcemaps.write('../maps'))
+		.pipe(dest(paths.sassDest))
+	done()
+}
+
+function javaScript(done) {
+	src(paths.js)
+		.pipe(sourcemaps.init())
+		.pipe(babel({ presets: ['@babel/env'] }))
+		.pipe(uglify())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(sourcemaps.write('../maps'))
+		.pipe(dest(paths.jsDest))
+	done()
+}
+
+function convertImages(done) {
+	src(paths.img).pipe(imagemin()).pipe(dest(paths.imgDest))
+	done()
+}
+
+function startBrowserSync(done) {
+	browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+	done()
+}
+
+const mainFunctions = parallel(sassCompiler, javaScript, convertImages)
+exports.default = series(mainFunctions, startBrowserSync)
